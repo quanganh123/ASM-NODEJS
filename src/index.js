be26c194e672b8744dff75e9538c66aab609cd72
Nguyễn Quang Anh <127297496+quanganh123@users.noreply.github.com>
2023-09-21 17:45:17 +0700

    Test

---
+++ src/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const morgan = require('morgan');
+const handlebars = require('express-handlebars');
+const app = express();
+const port = 3000;
+
+app.engine('hbs', handlebars.engine({
+    extname: '.hbs'
+}));
+app.set('view engine', 'hbs');
+// app.set('views', path.join(__dirname, 'resources/views'));
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+});
